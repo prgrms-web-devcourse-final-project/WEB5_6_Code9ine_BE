eceb608e89c189e67235706ae0081cd209f90df9
aronmin <scm0418@naver.com>
2025-07-09 11:34:41 +0900

    feat: 관리자 Mock API 및 DTO 구현
    - 관리자 모든 회원 조회
    - 관리자 유저 닉네임으로 유저 검색
    - 관리자 유저 차단
    - 관리자 모든 장소 조회
    - 관리자 카테고리별 장소 조회
    - 관리자 장소 등록
    - 관리자 장소 수정
    - 관리자 장소 삭제
    - 관리자 당일 통계 조회

---
+++ apimocking-main/apimocking-boilerplate/src/main/java/com/grepp/spring/app/model/admin/dto/AdminStoreMenuResponse.java
@@ -0,0 +1,16 @@
+package com.grepp.spring.app.model.admin.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "관리자 가게 메뉴 응답 DTO")
+public record AdminStoreMenuResponse(
+
+    @Schema(description = "메뉴 이름", example = "대패삼겹살")
+    String name,
+
+    @Schema(description = "메뉴 가격", example = "4500")
+    int price
+
+    ) {
+
+}

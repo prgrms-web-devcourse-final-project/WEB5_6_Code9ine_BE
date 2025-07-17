d1ba249c7d1aa53c5406d69e0853724be5314e39
doyun <youn6432@naver.com>
2025-07-17 17:41:35 +0900

    feature: 오늘지출없음 기능추가

--- apimocking-main/apimocking-boilerplate/src/main/java/com/grepp/spring/app/model/budget/repos/BudgetRepository.java
+++ apimocking-main/apimocking-boilerplate/src/main/java/com/grepp/spring/app/model/budget/repos/BudgetRepository.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
 
     Budget findFirstByMember(Member member);
+    Optional<Budget> findByMemberAndDate(Member member, LocalDate date);
 
     @Query(value = """
             SELECT SUM(total_income - total_expense) / COUNT(DISTINCT member_id)
